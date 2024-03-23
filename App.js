16c4b9fc52d7e1bfca0d7edf608e91f22586f8b0
Rodolfo96 <rodolfocosta.dev@gmail.com>
2024-03-23 11:32:49 -0300

    Restante dos arquivos

---
+++ App.js
@@ -0,0 +1,8 @@
+import ScoreBoardBasketball from './src/components/screens/Basketball';
+//import { CountdownTimerBall } from "./src/components/screens/Basketball/Cronometro/CronometroQuarter";
+export default function App() {
+  return (
+    <ScoreBoardBasketball />
+  );
+}
+
